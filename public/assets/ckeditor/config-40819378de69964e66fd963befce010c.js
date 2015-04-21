0f2cbfe933efbde6180ffbffb65c8bf0af04628a
rickymm3 <rickymm3@gmail.com>
2015-04-21 01:44:32 -0400

    precompiled assets for deploy

---
+++ public/assets/ckeditor/config-40819378de69964e66fd963befce010c.js
@@ -0,0 +1,15 @@
+if(typeof(CKEDITOR) != 'undefined')
+{
+    CKEDITOR.editorConfig = function(config) {
+        config.toolbar = [
+            { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'RemoveFormat' ] },
+            { name: 'links', items: [ 'Link'] },
+            { name: 'insert', items: [ 'Image'] },
+//            '/',
+            { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+            { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+        ];    }
+} else{
+    console.log("ckeditor not loaded")
+}
+;
